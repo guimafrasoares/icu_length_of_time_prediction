79e4b033efae1a894e247eaa63d6961e52483849
Guilherme <gmsoares11@icloud.com>
2025-12-27 20:19:15 -0300

    fix data_processing and classification evaluation

--- src/data_processing.py
+++ src/data_processing.py
@@ -18,13 +18,17 @@ def load_data(database_url):
 
     return df
 
-def preprocess_data(df):
+def preprocess_data(df, classification: bool):
+    # Round 'los' to 2 decimal places
+    df['los'] = df['los'].round(2)
+
     # Drop unecessary columns
     df_simplified = df.drop(['subject_id', 'hadm_id', 'stay_id', 'intime', 'outtime'], axis=1)
 
     # Filter out rows with 'NA' careunit
     df_subset = df_simplified.loc[df_simplified['careunit'] != 'NA']
-
+    df_subset = df_subset.dropna()
+    
     # Encode categorical variables
     label_encoder = LabelEncoder()
     df_encoded = df_subset.copy()
@@ -32,6 +36,16 @@ def preprocess_data(df):
 
     for col in cat_cols:
         df_encoded[col] = label_encoder.fit_transform(df_encoded[col])
+
+    if classification:
+        # For classification, label 'los' into categories
+        bins = [0, 1, 3, 7, float('inf')]
+        labels = [0, 1, 2, 3]  # 0: <1 day, 1: 1-3 days, 2: 3-7 days, 3: >7 days
+        df_encoded['los'] = pd.cut(df_encoded['los'], bins=bins, labels=labels, right=False)
+        
+        # Encode los column
+        label_encoder = LabelEncoder()
+        df_encoded['los'] = label_encoder.fit_transform(df_encoded['los'])
         
     return df_encoded
 
@@ -40,16 +54,17 @@ def split_data(df):
     X_train, X_test, y_train, y_test = train_test_split(dfx, df['los'], test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
 
-def get_prepared_data(database_url="postgresql://gms@localhost/mimiciv"):
+def get_prepared_data(database_url: str, classification: bool):
     """
     Get prepared data.
     
     Parameters:
     database_url (str): Database connection URL.
-
+    classification (bool): Whether to perform classification or regression.
+    
     Returns -> Tuple: X_train, X_test, y_train, y_test
     """
     df = load_data(database_url)
-    df_processed = preprocess_data(df)
+    df_processed = preprocess_data(df, classification)
     X_train, X_test, y_train, y_test = split_data(df_processed)
     return X_train, X_test, y_train, y_test
--- src/evaluation.py
+++ src/evaluation.py
@@ -1,7 +1,7 @@
 from sklearn.metrics import accuracy_score, roc_auc_score, matthews_corrcoef, balanced_accuracy_score, r2_score, f1_score
-from sklearn.metrics import  mean_absolute_error, mean_squared_error, root_mean_squared_error, mean_absolute_percentage_error
+from sklearn.metrics import  mean_absolute_error, root_mean_squared_error, mean_absolute_percentage_error
 
-def evaluate_classification_model(y_true, y_pred):
+def evaluate_classification_model(y_true, y_pred, y_proba):
     """
     Evaluate the performance of a classification model.
 
@@ -19,10 +19,9 @@ def evaluate_classification_model(y_true, y_pred):
     """
     accuracy = accuracy_score(y_true, y_pred)
     balanced_accuracy = balanced_accuracy_score(y_true, y_pred)
-    auc = roc_auc_score(y_true, y_pred)
-    f1 = f1_score(y_true, y_pred)
+    auc = roc_auc_score(y_true, y_proba, multi_class='ovr', average='macro')
+    f1 = f1_score(y_true, y_pred, average='macro')
     mcc = matthews_corrcoef(y_true, y_pred)
-
     return {
         'ACC': accuracy,
         'Balanced_ACC': balanced_accuracy,
