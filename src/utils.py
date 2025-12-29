289655e62329131905e89f9db3f395c18d61e719
Guilherme <gmsoares11@icloud.com>
2025-12-28 21:41:57 -0300

    create class with utilities

---
+++ src/utils.py
@@ -0,0 +1,52 @@
+import joblib
+import os
+from typing import Literal
+
+_file_extension = ".zlib"
+_default_path_classification = "./trained_models/classification/"
+_default_path_regression = "./trained_models/regression/"
+
+def save_model(model, filename: str, model_type: Literal["classification", "regression"]):
+    """
+    Save trained model to disk.
+    
+    Parameters:
+    model: The trained model to save.
+    filename (str): The name of the file to save the model to.
+    model_type: Type of the model - "classification" or "regression".
+    """
+    if model_type == "classification":
+        full_name = _default_path_classification + filename + _file_extension
+    elif model_type == "regression":
+        full_name = _default_path_regression + filename + _file_extension
+    else:
+        raise ValueError("Invalid model type. Use 'classification' or 'regression'.")
+
+    if os.path.exists(full_name):
+        os.remove(full_name)
+
+    with open(full_name, 'wb') as model_file:
+        joblib.dump(model, model_file, compress='zlib')
+
+
+def load_model(filename: str, model_type: Literal["classification", "regression"]):
+    """
+    Load trained model from disk.
+    
+    Parameters:
+    filename (str): Name of the file to load the model from.
+    model_type: Type of the model - "classification" or "regression".
+
+    Returns: Loaded model.
+    """
+    if model_type == "classification":
+        full_name = _default_path_classification + filename + _file_extension
+    elif model_type == "regression":
+        full_name = _default_path_regression + filename + _file_extension
+    else:
+        raise ValueError("Invalid model type. Use 'classification' or 'regression'.")
+    
+    with open(full_name, 'rb') as model_file:
+        model = joblib.load(model_file)
+
+    return model
