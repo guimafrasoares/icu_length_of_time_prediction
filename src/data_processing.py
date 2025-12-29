9dc73fe91ef7f2fc87e87168d4c21a3d80fe0df6
Guilherme <gmsoares11@icloud.com>
2025-12-28 21:41:32 -0300

    fix label for diagnosis columns

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sqlalchemy import create_engine
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
@@ -18,38 +19,48 @@ def load_data(database_url):
 
     return df
 
-def preprocess_data(df, classification: bool):
+def __preprocess_data(df, classification: bool):
     
-    # Drop unecessary columns
+    # Drop unecessary columns and rows with missing values
     df_simplified = df.drop(['subject_id', 'hadm_id', 'stay_id', 'intime', 'outtime'], axis=1)
+    df_simplified = df_simplified.dropna()
 
-    # Filter out rows with 'NA' careunit
-    df_subset = df_simplified.loc[df_simplified['careunit'] != 'NA']
-    df_subset = df_subset.dropna()
+    # Encode careunit column
+    df_encoded = df_simplified.copy()
+    careunit_mapping = {'NA': 0, 'MICU': 1, 'SICU': 2}
+    df_encoded['careunit'] = df_encoded['careunit'].map(careunit_mapping)
     
-    # Encode categorical variables
+    # Encode gender column
+    gender_mapping = {'M': 0, 'F': 1}
+    df_encoded['gender'] = df_encoded['gender'].map(gender_mapping)
+
+    # Encode diagnosis columns
+    diagnosis = np.concatenate((
+        df_encoded['diagnosis_1'].unique(),
+        df_encoded['diagnosis_2'].unique(),
+        df_encoded['diagnosis_3'].unique()
+    ))
+
     label_encoder = LabelEncoder()
-    df_encoded = df_subset.copy()
-    cat_cols = df_encoded.select_dtypes(include=['object']).columns
+    label_encoder.fit(diagnosis)
 
-    for col in cat_cols:
-        df_encoded[col] = label_encoder.fit_transform(df_encoded[col])
+    df_encoded['diagnosis_1'] = label_encoder.transform(df_encoded['diagnosis_1'])
+    df_encoded['diagnosis_2'] = label_encoder.transform(df_encoded['diagnosis_2'])
+    df_encoded['diagnosis_3'] = label_encoder.transform(df_encoded['diagnosis_3'])
 
+    # Encode target variable
     if classification:
         # For classification, label 'los' into 2 categories
         bins = [0, 7, float('inf')]
         labels = [0, 1]  # 0: <=7 days, 1: >7 days
         df_encoded['los'] = pd.cut(df_encoded['los'], bins=bins, labels=labels, right=True)
-        
-        # Encode los column
-        label_encoder = LabelEncoder()
-        df_encoded['los'] = label_encoder.fit_transform(df_encoded['los'])
+
     else:
-        df_encoded['los'] = df_encoded['los'].round(1)
-        
+        df_encoded['los'] = df_encoded['los'].round(2)
+
     return df_encoded
 
-def split_data(df):
+def __split_data(df):
     dfx = df.drop(['los'], axis=1)
     X_train, X_test, y_train, y_test = train_test_split(dfx, df['los'], test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
@@ -65,6 +76,6 @@ def get_prepared_data(database_url: str, classification: bool):
     Returns -> Tuple: X_train, X_test, y_train, y_test
     """
     df = load_data(database_url)
-    df_processed = preprocess_data(df, classification)
-    X_train, X_test, y_train, y_test = split_data(df_processed)
+    df_processed = __preprocess_data(df, classification)
+    X_train, X_test, y_train, y_test = __split_data(df_processed)
     return X_train, X_test, y_train, y_test
